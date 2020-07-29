b24236141ede94273ee70e01e75e2153483ad64b
Tommy <tommylee.edu@gmail.com>
2020-07-29 16:54:52 -0400

    Added Camera Follow + Camera Zoom

---
+++ Assets/PlayerFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    private Transform PlayerTransform;
+    public float cameraZoom = 3.0f;
+
+    void Start()
+    {
+        PlayerTransform = GameObject.Find("Player").transform;
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 20) / cameraZoom);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y, transform.position.z);
+    }
+}
