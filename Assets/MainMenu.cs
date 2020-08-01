7f00d458777c60edf166038b2acdce0604db567b
Thomas <thomasjnliu@gmail.com>
2020-08-01 14:37:32 -0400

    Add main menu music + SFX

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,14 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource audio;
+    public AudioSource bgm;
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        bgm.Stop();
+        audio.Play();
+        StartCoroutine("LoadLevel");
+
     }
 
     public void QuitGame()
     {
         Debug.Log("Quit Game");
         Application.Quit();
+    }
+
+    public IEnumerator LoadLevel(){
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("SampleScene");
     }
 }
